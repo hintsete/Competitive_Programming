a797563d28afb666cc9b76f3ab4ea58e287f6403
hintsete <167539044+hintsete@users.noreply.github.com>
2025-02-02 23:00:42 +0300

    Created 'Plus One' by A2SV Hub

---
+++ 02-Feb-2025/Plus One 235043.py
@@ -0,0 +1,9 @@
+# Problem: Plus One - https://leetcode.com/problems/plus-one/
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+      str_digits=list(map(str,digits))
+      joined="".join(str_digits)
+      num=int(joined)
+      num+=1
+      return [int(digit) for digit in str(num)]
