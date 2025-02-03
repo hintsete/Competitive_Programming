a88feec4d82c2c2046283b9aa70bfa5611ce29ad
hintsete <167539044+hintsete@users.noreply.github.com>
2025-02-03 17:14:10 +0300

    Created 'Valid Anagram' by A2SV Hub

---
+++ 03-Feb-2025/Valid Anagram 236648.py
@@ -0,0 +1,10 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count_1=Counter(s)
+        count_2=Counter(t)
+        if count_1==count_2:
+            return True
+        else:
+            return False
