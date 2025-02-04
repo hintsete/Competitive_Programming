09b4973483eca2c324bc17a76e90db39830e7a44
hintsete <167539044+hintsete@users.noreply.github.com>
2025-02-04 13:50:58 +0300

    Created 'Transformed Array' by A2SV Hub

---
+++ 04-Feb-2025/Transformed Array 239573.py
@@ -0,0 +1,14 @@
+# Problem: Transformed Array - https://leetcode.com/problems/transformed-array/description/
+
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        res=[]
+        n=len(nums)
+        for i in range(n):
+            if nums[i]>0:
+                res.append(nums[(i+nums[i])%n])
+            elif nums[i]<0 :
+                res.append(nums[(i+nums[i])%n])
+            else:
+                res.append(nums[i])
+        return res
