aca6e1abd2b89f40023ff330dce047f9372f037a
hintsete <167539044+hintsete@users.noreply.github.com>
2025-02-05 21:58:55 +0300

    Created 'Keyboard Row' by A2SV Hub

---
+++ 05-Feb-2025/Keyboard Row 244922.py
@@ -0,0 +1,19 @@
+# Problem: Keyboard Row - https://leetcode.com/problems/keyboard-row/description/
+
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        output=[]
+        first_row=set("qwertyuiop")
+        second_row=set("asdfghjkl")
+        third_row=set("zxcvbnm")
+
+        for word in words:
+            new_word=set(word.lower())
+            if new_word.issubset(first_row) or new_word.issubset(second_row) or new_word.issubset(third_row):
+                output.append(word)
+            
+           
+         
+
+        return output
+        
