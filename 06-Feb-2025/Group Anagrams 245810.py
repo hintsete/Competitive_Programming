1c42511ac23d7d99d7d1d4ad62e9676c34353d9c
hintsete <167539044+hintsete@users.noreply.github.com>
2025-02-06 10:23:29 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 06-Feb-2025/Group Anagrams 245810.py
@@ -0,0 +1,14 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram=defaultdict(list)
+        for s in strs:
+            s_sorted="".join(sorted(s))
+            anagram[s_sorted].append(s)
+
+        output=[]
+        for val in anagram.values():
+            output.append(val)
+        return output
+        
