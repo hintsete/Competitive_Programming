392f736d34e3df324c20d3b2e2f1204910c73c40
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-02-11 13:06:02 +0300

    Created 'Build Array from Permutation' by A2SV Hub

---
+++ 11-Feb-2025/Build Array from Permutation 256067.py
@@ -0,0 +1,10 @@
+# Problem: Build Array from Permutation - https://leetcode.com/problems/build-array-from-permutation/description/
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans=[0]*len(nums)
+        # print(ans)
+        for i in range(len(nums)):
+            ans[i]=nums[nums[i]]
+        return ans
+
