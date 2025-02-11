87b3666963f1e6063de1e60c42aa01d258be9a51
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-02-11 14:13:26 +0300

    Created 'Sort Characters By Frequency' by A2SV Hub

---
+++ 11-Feb-2025/Sort Characters By Frequency 256274.py
@@ -0,0 +1,19 @@
+# Problem: Sort Characters By Frequency - https://leetcode.com/problems/sort-characters-by-frequency/description/
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        count=Counter(s)
+        values=[]
+        for val in count.values():
+            values.append(val)
+        values.sort(reverse=True)
+        processed=set()
+        res=[]
+        for i in range(len(values)):
+            for key,val in count.items():
+                if val==values[i] and key not in processed:
+                    res.append(key*val)
+                    print(res)
+                    processed.add(key)
+        return "".join(res)
+                    
