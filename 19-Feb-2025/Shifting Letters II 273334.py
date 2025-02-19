6f196a18646f4a4ef9409b2f046db377a6517036
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-02-19 13:43:30 +0300

    Created 'Shifting Letters II' by A2SV Hub

---
+++ 19-Feb-2025/Shifting Letters II 273334.py
@@ -0,0 +1,30 @@
+# Problem: Shifting Letters II - https://leetcode.com/problems/shifting-letters-ii/description/
+
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        range_Sum=[0]*(len(s)+1)
+
+        for start,end,direction in shifts:
+            if direction==1:
+                range_Sum[start]+=1
+                if end+1<len(range_Sum):
+                    range_Sum[end+1]-=1
+                
+            else:
+                range_Sum[start]-=1
+                if end+1 < len(range_Sum):
+                    range_Sum[end+1]+=1
+         
+        for i in range(1,len(range_Sum)):
+            range_Sum[i]+=range_Sum[i-1]
+        
+        shifted_chars=[]
+        for i in range(len(s)):
+            shift=range_Sum[i]%26
+            new_val=ord(s[i])+shift
+            if new_val>122:
+                new_val-=26
+            if new_val<97:
+                new_val+=26
+            shifted_chars.append(chr(new_val))
+        return "".join(shifted_chars)
