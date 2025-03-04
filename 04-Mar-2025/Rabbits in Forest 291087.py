a407f9e56ef8428ac91ebe74ea567903f9204f80
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-04 16:25:59 +0300

    Created 'Rabbits in Forest' by A2SV Hub

---
+++ 04-Mar-2025/Rabbits in Forest 291087.py
@@ -0,0 +1,16 @@
+# Problem: Rabbits in Forest - https://leetcode.com/problems/rabbits-in-forest/
+
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+
+        count=Counter(answers)
+        ans=[]
+        for key,val in count.items():
+            if key==0 :
+                ans.append(val)
+            else:
+                group=(key+val)//(key+1)
+                ans.append(group*(key+1))
+        # print(ans)
+        
+        return sum(ans)
