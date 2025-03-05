edffbf0fcde77dc494df65b27af53ae5ccc51047
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-05 23:03:26 +0300

    Created 'Simplify Path' by A2SV Hub

---
+++ 05-Mar-2025/Simplify Path 293628.py
@@ -0,0 +1,17 @@
+# Problem: Simplify Path - https://leetcode.com/problems/simplify-path/
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack=[]
+        paths=path.split("/")
+        print(paths)
+        for p in paths:
+            if p=="" or p==".":
+                continue
+            elif  p==".." and stack:
+                stack.pop()
+
+            elif p!="..":
+                stack.append(p)
+        
+        return "/"+"/".join(stack)
