984f328020470e03750b479365452bebd6fea841
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-06 14:21:54 +0300

    Created 'Number of Recent Calls' by A2SV Hub

---
+++ 06-Mar-2025/Number of Recent Calls 294161.py
@@ -0,0 +1,13 @@
+# Problem: Number of Recent Calls - https://leetcode.com/problems/number-of-recent-calls/
+
+class RecentCounter:
+
+    def __init__(self):
+        self.queue=deque()
+        
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        while self.queue[0]<t-3000:
+            self.queue.popleft()
+        return len(self.queue)
