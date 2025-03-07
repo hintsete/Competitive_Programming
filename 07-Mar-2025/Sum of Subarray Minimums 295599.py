b5c44291836acb59f081e2f0e810853fe3ad2736
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-07 10:15:11 +0300

    Created 'Sum of Subarray Minimums' by A2SV Hub

---
+++ 07-Mar-2025/Sum of Subarray Minimums 295599.py
@@ -0,0 +1,21 @@
+# Problem: Sum of Subarray Minimums - https://leetcode.com/problems/sum-of-subarray-minimums/
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        total=0
+        queue=deque()
+        arr.append(0)
+        for i in range(len(arr)):
+            while queue and arr[i]<arr[queue[-1]]:
+                mid=queue.pop()
+                left=queue[-1] if queue else -1
+                right=i
+                count=(mid-left)*(right-mid)
+                total+=arr[mid]*count
+
+                
+                # queue.popleft()
+                
+            queue.append(i)
+            
+        return total%(10**9+7)
