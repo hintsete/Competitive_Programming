ce466746fdfd91672c1055578d5b8466517b263a
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-10 19:00:13 +0300

    Created 'Implement Stack using Queues' by A2SV Hub

---
+++ 10-Mar-2025/Implement Stack using Queues 300478.py
@@ -0,0 +1,25 @@
+# Problem: Implement Stack using Queues - https://leetcode.com/problems/implement-stack-using-queues/
+
+class MyStack:
+
+    def __init__(self):
+        self.stack=[]
+
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
+
+    def pop(self) -> int:
+        return self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def empty(self) -> bool:
+        if len(self.stack)==0:
+            return True
+        return False
