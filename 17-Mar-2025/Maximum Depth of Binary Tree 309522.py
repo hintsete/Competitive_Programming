a885b177e8e6e5ee73afece0af47968b4756029b
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-17 19:55:38 +0300

    Created 'Maximum Depth of Binary Tree' by A2SV Hub

---
+++ 17-Mar-2025/Maximum Depth of Binary Tree 309522.py
@@ -0,0 +1,17 @@
+# Problem: Maximum Depth of Binary Tree - https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def findMax(node):
+            if not node:
+                return 0
+            left=1+findMax(node.left)
+            right=1+findMax(node.right)
+            return max(left,right)
+        return findMax(root)
