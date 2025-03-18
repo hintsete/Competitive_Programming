2b5b8872f44e47fe26ae9001e4766cd8d83092d7
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-19 00:03:50 +0300

    Created 'Sum Root to Leaf Numbers' by A2SV Hub

---
+++ 18-Mar-2025/Sum Root to Leaf Numbers 311925.py
@@ -0,0 +1,28 @@
+# Problem: Sum Root to Leaf Numbers - https://leetcode.com/problems/sum-root-to-leaf-numbers/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue=deque()
+        queue.append([root,str(root.val)])
+        total=0
+
+        while queue:
+            # curr_val=""
+            for _ in range(len(queue)):
+                node,curr_val=queue.popleft()
+                if not node.left and not node.right:
+                    total+=int(curr_val)
+                if node.left:
+                    queue.append([node.left,curr_val+str(node.left.val)])
+                if node.right:
+                    queue.append([node.right,curr_val+str(node.right.val)])
+              
+        return total
