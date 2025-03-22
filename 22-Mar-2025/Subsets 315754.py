2ca6549037477c0bcccecfa28f5ad540501057ad
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-22 22:03:06 +0300

    Created 'Subsets' by A2SV Hub

---
+++ 22-Mar-2025/Subsets 315754.py
@@ -0,0 +1,15 @@
+# Problem: Subsets - https://leetcode.com/problems/subsets/
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        def dfs(element,path):
+            ans.append(path[:])
+
+            for i in range(element,len(nums)):
+                path.append(nums[i])
+                dfs(i+1,path)
+                path.pop()
+        dfs(0,[])
+        return ans
+            
