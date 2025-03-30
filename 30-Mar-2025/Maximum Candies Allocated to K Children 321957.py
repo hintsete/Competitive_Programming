c6a3e6190cc977f2726ad8abc82ef605152ad0cd
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-30 15:44:48 +0300

    Created 'Maximum Candies Allocated to K Children' by A2SV Hub

---
+++ 30-Mar-2025/Maximum Candies Allocated to K Children 321957.py
@@ -0,0 +1,20 @@
+# Problem: Maximum Candies Allocated to K Children - https://leetcode.com/problems/maximum-candies-allocated-to-k-children/
+
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        def valid(mid):
+            total=0
+            for c in candies:
+                total+=(c//mid)
+            return total>=k
+
+        l=1
+        r=sum(candies)
+        while l<=r:
+            mid=(l+r)//2
+            if valid(mid):
+                l=mid+1
+            else:
+                r=mid-1
+        return r
+        
