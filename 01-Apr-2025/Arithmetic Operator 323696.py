59ba3f9e2097f03a1917d53ce1f593b5f739a803
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-04-02 01:38:43 +0300

    Created 'Arithmetic Operator' by A2SV Hub

---
+++ 01-Apr-2025/Arithmetic Operator 323696.py
@@ -0,0 +1,13 @@
+# Problem: Arithmetic Operator - https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    add=a+b
+    sub=a-b
+    pro=a*b
+    
+    print(add)
+    print(sub)
+    print(pro)
