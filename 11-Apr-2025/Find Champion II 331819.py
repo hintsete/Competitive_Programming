675c496dc3dff5b67223946a0afcae83a070944a
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-04-11 18:07:55 +0300

    Created 'Find Champion II' by A2SV Hub

---
+++ 11-Apr-2025/Find Champion II 331819.py
@@ -0,0 +1,13 @@
+# Problem: Find Champion II - https://leetcode.com/problems/find-champion-ii/
+
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        filtered=set()
+        for u,v in edges:
+            filtered.add(v)
+        players=set(range(n))
+        winners=players-filtered
+        if len(winners)==1:
+            return winners.pop()
+        return -1
+      
